f3b36f87bc13f5cf9f79eda3a26472486b6e631c
rt4817 <joel.ag789@gmail.com>
2026-01-04 19:29:12 +0530

    Switched to minimal neovim
    I will need to switch to this eventually anyways. vim not having
    clipboard built in is a bummer. I don't wanna have to get vim-full. I'm
    fine with using nvim as long as I use tree-sitter and lsp some point in
    the future. I guess I finally found something too minimal for me...

--- init.lua
+++ init.lua
@@ -1,36 +1,63 @@
--- bootstrap lazy.nvim (plugin manager)
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", lazypath
-  })
-end
-vim.opt.rtp:prepend(lazypath)
 
--- Leader key
+-- =========================
+-- Minimal Neovim setup (Lua)
+-- =========================
+
+-- --- Leader key ---
 vim.g.mapleader = " "
 
--- Core options
+-- --- UI ---
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
-vim.opt.expandtab = true
+vim.opt.signcolumn = "auto"
+
+-- --- Syntax + Colorscheme ---
+-- Keep the order: termguicolors -> syntax on -> colorscheme
+vim.cmd("syntax on")
+vim.opt.background = "dark"     -- switch to "light" if your terminal theme is light
+vim.cmd("colorscheme desert")   -- built-in; use "default" if you prefer
+
+-- --- Transparency (inherit terminal bg; don't change foregrounds) ---
+local function hl(group, spec)
+  vim.api.nvim_set_hl(0, group, spec)
+end
+
+hl("Normal",        { bg = "none" })
+hl("NormalNC",      { bg = "none" })
+hl("NonText",       { bg = "none" })
+hl("EndOfBuffer",   { bg = "none" })
+
+-- --- Gutter/columns: transparent bg, readable numbers ---
+hl("LineNr",        { bg = "none", fg = "#c8c8c8" })
+hl("CursorLineNr",  { bg = "none", fg = "#ffffff" })
+hl("SignColumn",    { bg = "none" })
+hl("FoldColumn",    { bg = "none" })
+
+-- --- Indentation ---
+vim.opt.expandtab  = true
 vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.signcolumn = "yes"
+vim.opt.tabstop    = 2
 
--- Transparent background
-vim.cmd([[
-  hi Normal guibg=NONE ctermbg=NONE
-  hi NormalNC guibg=NONE ctermbg=NONE
-  hi NonText guibg=NONE ctermbg=NONE
-]])
+-- --- Clipboard ---
+vim.opt.clipboard = "unnamedplus"
 
--- Load plugins via lazy
-require("lazy").setup(require("plugins"))
+-- --- Delete without copying (black-hole) ---
+-- NOTE: This will prevent `d`/`dd` from filling registers.
+vim.keymap.set("n", "d",  '"_d', { noremap = true })
+vim.keymap.set("x", "d",  '"_d', { noremap = true })
+vim.keymap.set("n", "dd", '"_dd', { noremap = true })
 
--- Load keymaps
-require("keymaps")
+-- --- Cut entire line to system clipboard (dx) ---
+vim.keymap.set("n", "dx", '"+dd', { noremap = true })
 
+-- --- Sane defaults ---
+-- (Neovim ignores 'ttyfast', so we skip it)
+vim.opt.incsearch  = true
+vim.opt.hlsearch   = true
+vim.opt.ignorecase = true
+vim.opt.smartcase  = true
+vim.opt.hidden     = true
+vim.opt.swapfile   = false
+vim.opt.backspace  = { "indent", "eol", "start" }
+vim.opt.laststatus = 0
